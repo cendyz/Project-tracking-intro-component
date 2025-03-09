25931a3415d59ba28a39f22079173c441dbdd835
cendyz <arek57823@gmail.com>
2025-03-09 20:16:45 +0100

    Nav desktop

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,17 +2,18 @@
 	<nav class="container py-[5rem] px-[3.5rem] flex justify-between items-center">
 		<img :src="logo" alt="square with breaks" class="block w-[3rem]" />
 		<button type="button" @click="isOpen = !isOpen">
-			<img :src="isOpen ? close : burger" :alt="isOpen ? 'close menu' : 'open menu'" />
+			<img :src="isOpen ? burger : close" :alt="isOpen ? 'close menu' : 'open menu'" class="w-[3rem] lg:hidden" />
 		</button>
 		<Transition mode="out-in">
 			<div
-				v-show="isOpen"
-				class="absolute left-[3.5rem] top-[18%] bg-white shad w-[calc(100%-7rem)] p-[2rem] flex flex-col text-center rounded-md transition-opacity z-[100]">
+				v-show="isLargeScreen"
+				class="absolute left-[3.5rem] top-[18%] bg-white shad w-[calc(100%-7rem)] p-[2rem] flex flex-col text-center rounded-md transition-opacity z-[100] lg:bg-transparent lg:static lg:flex-row lg:p-0 lg:justify-end lg:gap-x-[7rem]">
 				<a
 					href="#"
 					v-for="(item, index) in links"
 					:key="index"
-					class="font-barloC font-w700 uppercase text-[2rem] tracking-[1px] pb-[2rem] last:text-neutral-blue-300 last:pt-[2rem] last:border-t-[1px] w-full"
+					class="font-barloC font-w700 uppercase text-[2rem] tracking-[1px] pb-[2rem] last:text-neutral-blue-300 last:pt-[2rem] last:border-t-[1px] w-full lg:last:p-0 lg:last:border-t-0 lg:p-0 lg:w-auto lg:last:ml-[3.5rem]"
+					:class="index == 3 && 'testDot'"
 					>{{ item }}</a
 				>
 			</div>
@@ -21,12 +22,22 @@
 </template>
 
 <script setup lang="ts">
-import { Ref, ref } from 'vue'
+import { Ref, ref, computed } from 'vue'
 import logo from '@/images/logo.svg'
 import burger from '@/images/icon-hamburger.svg'
 import close from '@/images/icon-close.svg'
+import { useWindowSize } from '@vueuse/core'
 
+const { width } = useWindowSize()
 const isOpen: Ref<boolean> = ref(false)
+const isLargeScreen = computed(() => {
+	if (width.value >= 1024) {
+		isOpen.value = false
+		return true
+	}
+	return !isOpen.value
+})
+
 const links: Ref<string[]> = ref(['product', 'features', 'pricing', 'login'])
 </script>
 
@@ -43,5 +54,27 @@ const links: Ref<string[]> = ref(['product', 'features', 'pricing', 'login'])
 .v-enter-from,
 .v-leave-to {
 	opacity: 0;
+}
+
+@media (min-width: 1024px) {
+	.shad {
+		box-shadow: none;
+	}
+
+	.testDot {
+		position: relative;
+
+		&::before {
+			position: absolute;
+			content: '';
+			top: 50%;
+			left: -5.5rem;
+			transform: translateY(-50%);
+			width: .7rem;
+			height: .7rem;
+			background-color: hsl(233, 6%, 72%);
+			border-radius: 50%;
+		}
+	}
 }
 </style>
